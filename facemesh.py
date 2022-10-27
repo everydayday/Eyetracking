5c3b8abb4f10886c2cdf29b684eb4f6ee27f43b4
Woong0416 <jack960416@gmail.com>
2022-10-27 12:03:47 +0900

    Update facemesh.py

--- facemesh.py
+++ facemesh.py
@@ -6,7 +6,7 @@ mp_face_mesh = mp.solutions.face_mesh
 
 
 
-# try1 
+# pa
 # 웹캠, 영상 파일의 경우 이것을 사용하세요.:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture(0)
